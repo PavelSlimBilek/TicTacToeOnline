a5c7fb6935f020b2c276656b75a39733453b156d
PavelSlimBilek <bilekpavel.p@gmail.com>
2024-08-25 12:25:45 +0200

    Start project

---
+++ TicTacToeOnline/src/main/java/eu/bilekpavel/tictactoeonline/TicTacToeOnlineApplication.java
@@ -0,0 +1,18 @@
+package eu.bilekpavel.tictactoeonline;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeOnlineApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeOnlineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Slim made :)");
+	}
+}
---
+++ TicTacToeOnline/src/test/java/eu/bilekpavel/tictactoeonline/TicTacToeOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.bilekpavel.tictactoeonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
